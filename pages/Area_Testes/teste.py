a971602669576a80da147b4a5d896a6fc1bf4d0b
Fellipe Almässy <fealmassy@gmail.com>
2026-03-15 09:21:22 -0300

    Update teste.py

--- pages/Area_Testes/teste.py
+++ pages/Area_Testes/teste.py
@@ -1,1 +1,28 @@
 
+import streamlit as st
+
+def divisores(lista: list, numero: int) -> tuple[list, list]:
+    
+    lista_divisores = []
+    lista_nao_divisores = []
+
+    if not isinstance(numero, int):
+        raise TypeError("o numero tem que ser inteiro")
+    if numero == 0:
+        raise ValueError("o num nao pode ser zero")
+    if len(lista) == 0:
+        raise ValueError("a lista nao pode estar vazia")
+    
+    i = 0
+    while i <= len(lista) - 1:
+        if not isinstance(lista[i], int):
+            raise ValueError("a lista tem que conter somente numeros")
+        if lista[i] % numero == 0:
+            lista_divisores.append(lista[i])
+        else:
+            lista_nao_divisores.append(lista[i])
+        i = i + 1
+
+    return lista_divisores, lista_nao_divisores
+
+st.title("adoro homens")
